fa7e709d1559bccb19dc92f0a23deaa233da20f0
Tahmid Chowdhury <tahmid1203@hotmail.com>
2025-05-05 00:39:03 -0400

    Simple player script

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
-func helloWorld():
-	print("Hello World!")
-
 # Runs once at startup
-func _ready():
-	helloWorld()
+#func _ready():
+	#...
 
 # Runs every frame
 func _process(delta: float) -> void:
-	print("test")
+	# Move position
+	#position.x += 1
+	#position.y += 1
+	
+	if Input.is_action_pressed("ui_right"):
+		position.x += 1
+	if Input.is_action_pressed("ui_left"):
+		position.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		position.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		position.y += 1
+	
+	pass
