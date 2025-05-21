c2a3480dc60cb5a584064c6409177a5fbd45de00
Tahmid Chowdhury <tahmid1203@hotmail.com>
2025-05-20 22:12:09 -0400

    Updated NPC dialogue

--- scenes/maps/TestScene/NPC_script.gd
+++ scenes/maps/TestScene/NPC_script.gd
@@ -4,21 +4,32 @@ extends StaticBody2D
 @export var player_node: Node2D
 @export var unique_text = ["Hello, I am the generated text!", "Hello, I am the generated text second box!" ]
 
+var has_queued: bool = false
+@export var cooldown_time: float = 2.0
+var cooldown_timer: float = 0.0
 
 func _ready() -> void:
 	if player_node == null:
 		var root = get_tree().get_current_scene()
 		player_node = root.find_child("Player", true, false)
-		set_process(true)
+	set_process(true)
 
 func _process(delta: float) -> void:
 	if player_node == null:
 		return
-	var distance = npc_node.global_position.distance_to(player_node.global_position)
-	if Input.is_action_just_pressed("z"):
-		if distance < 20 :
-			generate_unique_test() 
 		
+	var distance = npc_node.global_position.distance_to(player_node.global_position)
+	
+	if has_queued:
+		if not TextBox.is_displaying():
+			cooldown_timer += delta
+			if cooldown_timer >= cooldown_time:
+				has_queued = false
+				cooldown_timer = 0.0
+		
+	if Input.is_action_just_pressed("z") and TextBox.is_displaying() and distance < 20 and not has_queued:
+		generate_unique_test()
+		has_queued = true
 	
 func generate_unique_test():
 	for text in unique_text:
